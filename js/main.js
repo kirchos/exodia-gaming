fc8343dbf3e89c5a3f8808a52e42200f4a59177f
kirchos <stoimenov.kircho@gmail.com>
2022-09-23 15:33:50 +0200

    finished mobile first

--- js/main.js
+++ js/main.js
@@ -1,15 +1,36 @@
-// let question = document.querySelector('.question')
+let question1 = document.querySelector('.question-1');
+let answer1 = document.querySelector('.answer-1');
+
+let question2 = document.querySelector('.question2');
+let answer2 = document.querySelector('.answer2');
+
+let question3 = document.querySelector('.answer3');
+let answer3 = document.querySelector('.answer3');
+
+let question4 = document.querySelector('.answer3');
+let answer4 = document.querySelector('.answer3');
+
+let rules = document.getElementsByClassName('rule');
 
 
-// question.onclick = function () {
-//     // e.preventDefault()
-//     let answerDiv = document.createElement('div');
-//     answerDiv.classList.add('answer');
 
-//     let answerP = document.createElement('p');
-//     answerP.innerText = 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quod?';
-//     answerP.classList.add('answer-p');
+question1.addEventListener('click', function (e) {
+    answer1.classList.toggle('inactive');
+    let marketPicture = document.createElement('img');
+    marketPicture.classList = 'market-map';
+    marketPicture.src = './images/market map.jpg';
+    answer1.textContent = "The market is lotcated in the desert ";
+    answer1.append(marketPicture);
 
-//     question.append(answerP);
+    // e.stopPropagation()
+});
 
-// }
+// question2.addEventListener('click', function (e) {
+
+// })
+
+
+
+
+
+
