bc5ac2ac304a04bb839d0032310da0df846b7bf0
kirchos <stoimenov.kircho@gmail.com>
2022-09-23 12:11:36 +0200

    css added for  mobile

--- js/main.js
+++ js/main.js
@@ -1,1 +1,15 @@
-alert('Under Construction...')
+// let question = document.querySelector('.question')
+
+
+// question.onclick = function () {
+//     // e.preventDefault()
+//     let answerDiv = document.createElement('div');
+//     answerDiv.classList.add('answer');
+
+//     let answerP = document.createElement('p');
+//     answerP.innerText = 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quod?';
+//     answerP.classList.add('answer-p');
+
+//     question.append(answerP);
+
+// }
